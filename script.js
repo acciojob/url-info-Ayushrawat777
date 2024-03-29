656c96665f395d48e29cd2c90c1e3e96cad1e166
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-03-29 17:48:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 let text = window.location.href;
     
-      let pattern = /[a-z]/g;
-      let result = text.match(pattern).length;
+ //     let pattern = /[a-z]/g;
+      let result = text.length;
 
     window.alert("The length of the URL is:"+result)
